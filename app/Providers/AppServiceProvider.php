17dbd16e51d820795a5ac31372d9cd7d3416678e
Raphael Falcao <raphael.falcao@febrafar.com.br>
2024-12-06 15:54:43 -0300

    feat/pagamento

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use TechChallenge\Infra\DB\Eloquent\{Order\Model as Order};
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Factory::guessFactoryNamesUsing(function ($modelName) {            
+            if ($modelName === Order::class) {
+                return 'Database\\Factories\\OrderFactory';
+            }
+            return 'Database\\Factories\\' . class_basename($modelName) . 'Factory';
+        });
     }
 }
